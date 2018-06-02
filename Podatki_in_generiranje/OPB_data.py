364cf6fabb4a84c8f9be5beb62ae0848a7c032cd
Gal Bumbar <gal.bumbar@gmail.com>
2018-06-02 16:59:55 +0200

    Fixes #4

--- Podatki_in_generiranje/OPB_data.py
+++ Podatki_in_generiranje/OPB_data.py
@@ -1,18 +1,43 @@
 import pandas
+import csv
 import numpy as np
 import random
+import psycopg2
+import psycopg2.extensions
+import psycopg2.extras
+import hashlib
+import auth as auth
+from bottle import *
+# Povezovanje na bazo#
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)  # se znebimo problemov s šumniki
+# priklopimo se na bazo
+conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
+conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)  # onemogočimo transakcije
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+# poženemo strežnik na portu 8080, glej http://localhost:8080/
+# run(host='localhost', port=8080, reloader=True)
+
+# Slovarji za lažje urejanje
+
 Jeziki = {1: 'Slovensko', 2: 'Angleško', 3: 'Nemško', 4: 'Italjansko', 5: 'Francosko', 6: 'Špansko'}
 Igre = {1: 'Runescape', 2: 'Chess', 3: 'Skyrim', 4: 'Fornite',
         5: 'Super Mario', 6: 'PlayerUnknown''s Battlegrounds'}
 Platforme = {1: 'PC', 2: 'Playstation 4', 3: 'XBOX One', 4: 'Nintendo Switch'}
-Klani = {1: 'Marijani', 2: 'Divjaki', 3: 'Bratovščina sinjega galeba',
-         4: 'Gams in pivo'}  # Koliko klanov?
+# Klani = {1: 'Marijani', 2: 'Divjaki', 3: 'Bratovščina sinjega galeba',
+#          4: 'Gams in pivo'}  # Koliko klanov?
 # za vsako igro tabela vlog.
-Vloge = {1: ['Skiller', 'Pker', 'PvMer'], 2: ['Novice', 'Jaša master', 'National master', 'Master', 'Grandmaster'], 3: ['Speed runner', 'Explorer', 'Casual player'],
-         4: ['Team player', 'Solo player'], 5: ['Speed runner', 'Casual player'], 6: ['Solo player', 'Team player']}  # morava dodat se vloge v bazo?
-Opcije_komunikacije = {'Skype', 'Discord', 'Teamspeak'}  # katere opcije komunikacije?
+Vloge = {1: 'Skiller', 2: 'Pker', 3: 'PvMer', 4: 'Team player',
+         5: 'Solo player', 6: 'Speed runner', 7: 'Casual player', 8: 'Explorer', 9: 'Novice', 10: 'Expert', 11: 'National Master', 12: 'International Master', 13: 'Grandmaster'}
+Vloge_v_igri = {1: [1, 2, 3], 2: [9, 10, 11, 12, 13],
+                3: [6, 8, 7], 4: [4, 5], 5: [6, 7],
+                6: [5, 4]}  # morava dodat se vloge v bazo?
+Opcije_komunikacije = {1: 'Skype', 2: 'Discord', 3: 'Teamspeak'}  # katere opcije komunikacije?
 # To, katera igra je katere zvrsti in katere vloge so v kateri igri bova pac morala peš nrdit, ostalo lahko generirava.
 Zvrst = {1: 'MMORPG', 2: 'Strategy', 3: 'RPG', 4: 'TPS', 5: 'Arcade', 6: 'FPS'}
+Zvrsti_v_igrah = {'Runescape': 'MMORPG', 'Chess': 'Strategy', 'Skyrim': 'RPG',
+                  'Fornite': 'TPS', 'Super Mario': 'Arcade', 'PlayerUnknown''s Battlegrounds': 'FPS'}
+Na_Platformi = {1: [1], 2: [1, 2, 3], 3: [1, 2, 3, 4],
+                4: [1, 2, 3], 5: [1, 4], 6: [1, 2, 3]}
 
 stevilo_uporabnikov = 1000
 
@@ -21,45 +46,95 @@ stevilo_uporabnikov = 1000
 #    return fn(string)
 #
 
-randgen = np.random.randint(3, size=1000)
-gms = []
-for i in randgen:
-    gms = np.append(gms, Igre[i])
-gms
-data = pandas.read_csv("/Users/Gal/Desktop/OPB/MOCK_DATA.csv")
-data = data.assign(Games=gms)
-data.to_csv("/Users/Gal/Desktop/OPB/test.csv", index=False)
-data_test = pandas.read_csv("/Users/Gal/Desktop/OPB/test.csv")
-data_test
+osebe_in = open("osebe_data.csv")
+racuni_in = open("racuni_data.csv")
+osebe = csv.reader(osebe_in)
+next(osebe, None)
+racuni = csv.reader(racuni_in)
+next(racuni, None)
 
-# za generirange SQL stavkov za govorce
-with open('govorci.txt', 'a') as govorci:
-    for i in range(stevilo_uporabnikov):
-        for k, v in Jeziki.items():
-            if bool(random.getrandbits(1)):
-                govorci.write('INSERT INTO govorec (id, id) VALUES (%d, %d);\n' % (i, k))
 
-# generiranje SQL stavkov za igralce
-with open('igralci.txt', 'a') as igralci:
-    for i in range(stevilo_uporabnikov):
-        for k, v in Igre.items():
-            if bool(random.getrandbits(1)):
-                nr = len(Vloge[k])
-                st = random.randint(1, nr)
-                platrand = random.randint(1, 4)
-                igralci.write(
-                    'INSERT INTO igralec (igralecid, igraid, vloga, platforma) VALUES (%d, %d, %d);\n' % (i, k, Vloge[k, st], platrand))
+def hashano_geslo(s):
+    h = hashlib.md5()
+    h.update(s.encode('utf-8'))
+    return h.hexdigest()
 
-#  generiranje SQL stavkov za dodeljevanje v Klane, lahko dodava se opcijo da ni v klanu, treba se dopolnit s sevilom klanov.
 
-with open('pripadniki.txt', 'a') as pripadniki:
-    for i in range(stevilo_uporabnikov):
-        pripadniki.write('INSERT INTO pripadnik (id, id) VALUES (%d, %d);\n' % (i, k))
+# vstavljenje v osebe
+for rows in osebe:
+    cur.execute('INSERT INTO oseba(ime_oseba, priimek_oseba, casovni_pas) VALUES ( %s, %s, %s)', [
+                rows[0], rows[1], rows[2]])
 
-# generiranje SQL stavkov za dodeljevanje moznosti komunikacije
+# vstavljenje v račune
+for rows in racuni:
+    cur.execute('INSERT INTO racun(uporabnisko_ime, id_lastnik, geslo_hash) VALUES (%s, %s, %s)', [
+                rows[0], rows[1], hashano_geslo(rows[2])])
 
-with open('komunikator.txt', 'a') as komunikator:
-    for i in range(stevilo_uporabnikov):
-        for k, v in Opcije_komunikacije.items():
-            if bool(random.getrandbits(1)):
-                komunikator.write('INSERT INTO komunikator (id, id) VALUES (%d, %d);\n' % (i, k))
+# vstavljanje v zvrsti
+for k, v in Zvrst.items():
+    cur.execute('INSERT INTO zvrst(id_zvrst, ime_zvrst) VALUES (%s, %s)', [k, v])
+
+# vstavljanje v Vloge
+
+for k, v in Vloge.items():
+    cur.execute('INSERT INTO vloga(id_vloga, ime_vloga) VALUES (%s, %s)', [k, v])
+
+# vstavljanje v igralec
+
+for k, v in Igre.items():
+    cur.execute('INSERT INTO igra(id_igra, ime_igra, zvrst) VALUES (%s, %s, %s)', [k, v, k])
+
+# vstavljanje v ima_vlogo
+
+for k, v in Vloge_v_igri.items():
+    for j in v:
+        cur.execute('INSERT INTO ima_vlogo(igra, vloga) VALUES (%s, %s)', [k, j])
+
+# vstavljanja v platforma
+
+for k, v in Platforme.items():
+    cur.execute('INSERT INTO platforma(id_platforma, ime_platforma) VALUES (%s, %s)', [k, v])
+
+# vstavljanje v na_platformi
+
+for k, v in Na_Platformi.items():
+    for j in v:
+        cur.execute('INSERT INTO na_platformi(igra, platforma) VALUES (%s, %s)', [k, j])
+
+# vstavljanje v jeziki
+
+for k, v in Jeziki.items():
+    cur.execute('INSERT INTO jezik(id_jezik, ime_jezik) VALUES (%s, %s)', [k, v])
+
+# vstavljanje v opicje_komunikacije
+
+for k, v in Opcije_komunikacije.items():
+    cur.execute(
+        'INSERT INTO opcija_komunikacije(id_komunikacija, ime_komunikacija) VALUES (%s, %s)', [k, v])
+
+
+# vstavljanje v komunikator
+
+for i in range(1, stevilo_uporabnikov + 1):
+    for k, v in Opcije_komunikacije.items():
+        if bool(random.getrandbits(1)):
+            cur.execute(
+                'INSERT INTO komunikator(komunikator, opcija_kom) VALUES (%s, %s)', [i, k])
+
+# vstavljanje v govorec
+
+for i in range(1, stevilo_uporabnikov + 1):
+    for k, v in Jeziki.items():
+        if bool(random.getrandbits(1)):
+            cur.execute(
+                'INSERT INTO govorec(govorec, jezik) VALUES (%s, %s)', [i, k])
+
+# vstavljanje v igralec
+
+for i in range(1, stevilo_uporabnikov + 1):
+    for k, v in Igre.items():
+        if bool(random.getrandbits(1)):
+            vloga = random.randint(1, len(Vloge_v_igri[k]))
+            platforma = random.randint(1, 4)
+            cur.execute(
+                'INSERT INTO igralec(igralec, igra, vloga, platforma) VALUES (%s, %s, %s, %s)', [i, k, vloga, platforma])
---
+++ Podatki_in_generiranje/tabele.sql
@@ -0,0 +1,105 @@
+DROP TABLE IF EXISTS oseba CASCADE;
+DROP TABLE IF EXISTS racun CASCADE;
+DROP TABLE IF EXISTS igra CASCADE;
+DROP TABLE IF EXISTS platforma CASCADE;
+DROP TABLE IF EXISTS na_platformi CASCADE;
+DROP TABLE IF EXISTS jezik CASCADE;
+DROP TABLE IF EXISTS zvrst CASCADE;
+DROP TABLE IF EXISTS klan CASCADE;
+DROP TABLE IF EXISTS opcija_komunikacije CASCADE;
+DROP TABLE IF EXISTS komunikator CASCADE;
+DROP TABLE IF EXISTS igralec CASCADE;
+DROP TABLE IF EXISTS govorec CASCADE;
+DROP TABLE IF EXISTS pripadnik CASCADE;
+DROP TABLE IF EXISTS vloga CASCADE;
+DROP TABLE IF EXISTS ima_vlogo CASCADE;
+
+
+CREATE TABLE oseba (
+id_oseba SERIAL PRIMARY KEY,
+ime_oseba TEXT NOT NULL,
+priimek_oseba TEXT NOT NULL,
+casovni_pas TEXT NOT NULL
+);
+
+CREATE TABLE racun (
+id_racun SERIAL PRIMARY KEY,
+uporabnisko_ime TEXT NOT NULL,
+id_lastnik INTEGER UNIQUE NOT NULL REFERENCES oseba(id_oseba),
+geslo_hash TEXT NOT NULL
+);
+
+CREATE TABLE zvrst (
+id_zvrst INTEGER PRIMARY KEY,
+ime_zvrst TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE vloga (
+id_vloga INTEGER PRIMARY KEY,
+ime_vloga TEXT NOT NULL
+);
+
+
+CREATE TABLE igra (
+id_igra INTEGER PRIMARY KEY,
+ime_igra TEXT NOT NULL,
+zvrst INTEGER NOT NULL REFERENCES zvrst(id_zvrst)
+);
+
+CREATE TABLE ima_vlogo (
+igra INTEGER REFERENCES igra(id_igra),
+vloga INTEGER REFERENCES vloga(id_vloga),
+PRIMARY KEY (igra, vloga)
+);
+
+CREATE TABLE platforma (
+id_platforma INTEGER PRIMARY KEY,
+ime_platforma TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE na_platformi (
+igra INTEGER REFERENCES igra(id_igra),
+platforma INTEGER REFERENCES platforma(id_platforma)
+);
+
+CREATE TABLE jezik (
+id_jezik INTEGER PRIMARY KEY,
+ime_jezik TEXT UNIQUE NOT NULL
+);
+
+
+
+CREATE TABLE opcija_komunikacije (
+id_komunikacija INTEGER PRIMARY KEY,
+ime_komunikacija TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE komunikator (
+komunikator INTEGER REFERENCES racun(id_racun),
+opcija_kom INTEGER REFERENCES opcija_komunikacije(id_komunikacija),
+PRIMARY KEY(komunikator, opcija_kom)
+);
+
+CREATE TABLE igralec (
+igralec INTEGER REFERENCES racun(id_racun),
+igra INTEGER REFERENCES igra(id_igra),
+vloga INTEGER REFERENCES vloga(id_vloga),
+platforma INTEGER REFERENCES platforma(id_platforma),
+PRIMARY KEY (igralec, igra, vloga, platforma)
+-- CONSTRAINT igra_vloga CHECK(ima_vlogo(igra, vloga) IS NOT NULL),
+-- CONSTRAINT igra_platforma CHECK(na_platformi(igra, platforma) IS NOT NULL)
+);
+
+CREATE TABLE govorec (
+govorec INTEGER REFERENCES racun(id_racun),
+jezik INTEGER REFERENCES jezik(id_jezik),
+PRIMARY KEY(govorec, jezik)
+);
+
+
+GRANT ALL ON ALL TABLES IN SCHEMA public TO galb;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO vidk;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO galb WITH GRANT OPTION;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO vidk WITH GRANT OPTION;
+GRANT CONNECT ON DATABASE sem2018_galb TO javnost;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO javnost;
