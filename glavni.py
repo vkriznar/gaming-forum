47e57ccc34fef3f8881f310a8a5db081cd78467a
vkriznar <38419700+vkriznar@users.noreply.github.com>
2018-06-03 21:58:10 +0200

    Add files via upload
    dodal /sah in da se doda v bazo igra ce jo uporabnik zeli dodati

--- glavni.py
+++ glavni.py
@@ -12,14 +12,14 @@ cur = baza.cursor(cursor_factory=psycopg2.extras.DictCursor)
 secret = "56 Od nékdej lepé so Ljubljanke slovele, al lepši od Urške bilo ni nobene 56"
 static_directory = "./startbootstrap-modern-business-gh-pages"
 
-#username: chenker0
-#password: v1202y
+
 
 """Vsa gesla bodo za-hashana, pomozna funkcija za to"""
 def hashano_geslo(s):
     h = hashlib.md5()
     h.update(s.encode('utf-8'))
     return h.hexdigest()
+
 
 """Dobimo uporabnika iz piskotka, ce ga ni, ga vrzemo na login stran"""
 def get_user(auto_login = True, auto_redir=False):
@@ -64,7 +64,7 @@ def login_post():
     username = request.forms.username
     password = hashano_geslo(request.forms.password)
     c = baza.cursor()
-    c.execute("SELECT * FROM racun WHERE uporabnisko_ime=%s AND geslo=%s", [username, password])
+    c.execute("SELECT * FROM racun WHERE uporabnisko_ime=%s AND geslo_hash=%s", [username, password])
     table_users = c.fetchone()
     N = 6 #Koliko je iger
     if table_users is None:
@@ -87,25 +87,54 @@ def index():
     c.execute("SELECT * FROM racun WHERE uporabnisko_ime=%s", [username])
     table_users = c.fetchone()
     N = 6  # Koliko je iger
-    igre = []
+    igre = [False, False, False, False, False, False]
     ID = table_users[0]
-    c.execute("""SELECT igraID FROM igralec
-                        JOIN racun ON igralec.id_igralec=racun.id
-                        WHERE racun.ID=%s""", [ID])
-    for i in range(0, N):
+    c.execute("""SELECT igra FROM igralec
+                        JOIN racun ON igralec.igralec=racun.id_racun
+                        WHERE racun.id_racun=%s""", [ID])
+    x = c.fetchone()
+    while x is not None:
+        for i in range(0, N):
+            if not igre[i]:
+                if x[0] == i+1:
+                    igre[i] = True
         x = c.fetchone()
-        if x is not None:
-            if x[0] == i + 1:
-                igre.append(True)
-            else:
-                igre.append(False)
-        if len(igre) < i + 1:
-            igre.append(False)
     return template("index.html", user=username, igre=igre)
 
 
 @get("/index/kontakt/")
 def kontakt():
     return template("kontakt.html")
+
+@get("/index/sah/")
+def sah():
+    username = request.get_cookie('username', secret=secret)
+    c = baza.cursor()
+    c.execute("SELECT * FROM racun WHERE uporabnisko_ime=%s", [username])
+    table_users = c.fetchone()
+    N = 6  # Koliko je iger
+    igre = [False, False, False, False, False, False]
+    ID = table_users[0]
+    c.execute("""SELECT igra FROM igralec
+                            JOIN racun ON igralec.igralec=racun.id_racun
+                            WHERE racun.id_racun=%s""", [ID])
+    x = c.fetchone()
+    while x is not None:
+        for i in range(0, N):
+            if not igre[i]:
+                if x[0] == i + 1:
+                    igre[i] = True
+        x = c.fetchone()
+    return template("sah.html", igre=igre)
+
+@get("/index/sah/add/")
+def add():
+    username = request.get_cookie('username', secret=secret)
+    c = baza.cursor()
+    c.execute("SELECT id_racun FROM racun WHERE uporabnisko_ime=%s", [username])
+    id = c.fetchone()[0]
+    c.execute("""INSERT INTO igralec (igralec, igra, vloga, platforma)
+                    VALUES (%s, 2, 9, 1)""", [id])
+    redirect('/index/sah/')
 
 run(host='localhost', port=8080)
